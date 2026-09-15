a5bf20daa86e4f2ae574781edfa7485824351843
laorentou666 <oooops388@gmail.com>
2026-09-15 19:42:39 +0800

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,40 @@
 import java.util.Scanner;
-class DecimalToBinary {
+
+public class Main {
+    public static void showMenu(){
+        System.out.println("1.添加图书");
+        System.out.println("2.删除图书");
+        System.out.println("3.查询图书");
+        System.out.println("4.显示所有图书");
+        System.out.println("0.退出");
+        System.out.print("请输入\n");
+    }
     public static void main(String[] args) {
-        System.out.println("Enter a declimal number: ");
-        Scanner input = new Scanner(System.in);
-        int num = input.nextInt();
-        String binary = Integer.toBinaryString(num);
-        System.out.println(binary);
+        while(true){
+            showMenu();
+            Scanner sc = new Scanner(System.in);
+            int input = sc.nextInt(); //这里相当于scanf
+            switch (input){
+                case 1:
+                    System.out.println("添加图书有关代码");
+                    break;
+                case 2:
+                    System.out.println("删除图书有关代码");
+                    break;
+                case 3:
+                    System.out.println("查询图书有关代码");
+                    break;
+                case 4:
+                    System.out.println("所有图书");
+                    break;
+                case 0:
+                    System.exit(0);
+                    break;
+                default:
+                    sc.close();
+                    return;
+            }
+        }
+
     }
 }
