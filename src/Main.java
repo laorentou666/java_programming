19630d9e370e0676703a7fe6e906fd31fc309a92
laorentou666 <oooops388@gmail.com>
2026-09-15 20:42:29 +0800

    对象学习

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
-
+class Book{
+    String name;
+    String author;
+    float price;
+    int stock;
+}
 public class Main {
     public static void showMenu(){
         System.out.println("1.添加图书");
@@ -16,7 +21,15 @@ public class Main {
             int input = sc.nextInt(); //这里相当于scanf
             switch (input){
                 case 1:
-                    System.out.println("添加图书有关代码");
+                    Book temp = new Book();
+                    System.out.printf("temp默认名称%s\n",temp.name);
+                    System.out.printf("temp默认作者%s\n",temp.author);
+                    System.out.printf("temp默认价格%f\n",temp.price);
+                    System.out.printf("temp默认库存%d\n",temp.stock);
+                    Book a = new Book();
+                    Book b = a;
+                    b.price = 100f;
+                    System.out.printf("a的价格%f\n",a.price); //100
                     break;
                 case 2:
                     System.out.println("删除图书有关代码");
